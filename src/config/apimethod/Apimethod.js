1f6f7714f61ba269baded219ffd249f2cf4bf437
mabdullah <mabdullah2037@gmail.com>
2023-03-19 15:32:31 +0500

    api method added

---
+++ src/config/apimethod/Apimethod.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const baseurl = "https://jsonplaceholder.typicode.com/"
+
+
+//Get request
+
+const Get = async (apiName, id, params) => {
+    return await new Promise((resolve, reject) => {
+        axios.get(`${baseurl}${apiName}${id ? "/" + id : ""}`, { params: params })
+            .then((res) => {
+                resolve(res.data)
+            })
+            .catch((err) => {
+                reject(err)
+            })
+    })
+}
+
+//Post Request
+
+const Post = async (apiName, body) => {
+    return await new Promise((resolve, reject) => {
+        axios.post(`${baseurl}${apiName}`, body)
+            .then((res) => {
+                resolve(res.data)
+            })
+            .catch((err) => {
+                reject(err)
+            })
+    })
+}
+
+//Put Request
+
+let Put = async (apiName, body, id) => {
+    return await axios.put(`${baseApi}${apiName}/${id}`, body);
+};
+
+
+//Delete Request
+
+let Delete = async (apiName, id) => {
+    return await axios.delete(`${baseApi}${apiName}/${id}`);
+};
+
+export {Get , Post , Put , Delete};
